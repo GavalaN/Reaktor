f90c28d5b9433c8df1b61f901fe53e6902632f8e
GavalaN <gavalan@kkszki.hu>
2024-10-07 11:55:34 +0200

    asd

--- AtomReaktor/AtomReaktor/MainWindow.xaml.cs
+++ AtomReaktor/AtomReaktor/MainWindow.xaml.cs
@@ -21,6 +21,50 @@ namespace AtomReaktor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool aktiv_e = false;
+
+        static Random rnd = new Random();
+        static int hom;
+        static double aram;
+
+        public int Hom { get => Hom; private set => rnd.Next(40, 101); }
+        public double Aram { get => aram; set => aram = rnd.NextDouble() * (2.5 - 1) + 1; }
+
+        public void HutovizBenged()
+        {
+            hom = 40;
+        }
+
+        public void GenealtEnergia()
+        {
+            Aram = rnd.NextDouble() * (aram * 2 - aram) + aram;
+        }
+
+        public static bool Leallitas()
+        {
+            if (hom >= 70)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri("pack://application:,,,/Images/felrobbant.jpg", UriKind.Absolute);
+                bitmap.EndInit();
+
+                imgReaktor.Source = bitmap;
+                
+                return false;
+            }
+            else
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri("pack://application:,,,/Images/ep.jpg", UriKind.Absolute);
+                bitmap.EndInit();
+
+                imgReaktor.Source = bitmap;
+                return true;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,9 +82,10 @@ namespace AtomReaktor
 
         private void btnBeinditas_Click(object sender, RoutedEventArgs e)
         {
+            aktiv_e = true;
             BitmapImage bitmap = new BitmapImage();
             bitmap.BeginInit();
-            bitmap.UriSource = new Uri("pack://application:,,,/Images/felrobbant.jpg", UriKind.Absolute);
+            bitmap.UriSource = new Uri("pack://application:,,,/Images/ep.jpg", UriKind.Absolute);
             bitmap.EndInit();
 
             imgReaktor.Source = bitmap;
@@ -60,22 +105,23 @@ namespace AtomReaktor
 
         private void btnLeallitas_Click(object sender, RoutedEventArgs e)
         {
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri("pack://application:,,,/Images/ep.jpg", UriKind.Absolute);
-            bitmap.EndInit();
-
-            imgReaktor.Source = bitmap;
+            Leallitas();
         }
 
         private void btnGeneraltEnergia_Click(object sender, RoutedEventArgs e)
         {
-
+            if (aktiv_e)
+            {
+                GenealtEnergia();
+            }
         }
 
         private void btnHutovizBeengedese_Click(object sender, RoutedEventArgs e)
         {
-
+            if (aktiv_e)
+            {
+                HutovizBenged();
+            }
         }
 
         private void btnKilepes_Click(object sender, RoutedEventArgs e)
